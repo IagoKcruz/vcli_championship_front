610d2fa42f802179c22b1b2d5e4b7b214a1933e6
SCS192717\Aluno <iagokniphoff.ngc@gmail.com>
2024-02-14 20:37:32 -0300

    alteração

--- pageAdmin/script.js
+++ pageAdmin/script.js
@@ -20,16 +20,7 @@ actions()
 
 function tableTeams() {
     tableTeamsUl.insertAdjacentHTML("afterbegin", `
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
+    <li>GRE (Gremio)</li>
     `)
     tableTeamsUl.setAttribute("style", "padding:5px; margin-bottom: 10px;")
 }
