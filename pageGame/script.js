520335d8596b7e5369e4be97db6b759feca8a57f
SCS192717\Aluno <iagokniphoff.ngc@gmail.com>
2024-02-19 20:44:23 -0300

    game/2-IK

--- pageGame/script.js
+++ pageGame/script.js
@@ -3,49 +3,6 @@ const main = document.querySelector("main")
 function openGame(game){
 
 main.insertAdjacentHTML("afterbegin",`
-<section id="teams">
-<div id="teamHome">
-    <div>
-        <p>${game.TeamHome}</p>
-        <p>${game.GoalHome}</p>
-    </div>
-</div>
-<span>X</span>
-<div id="teamAwai">
-    <div>
-        <p>${game.TeamAwai}</p>
-        <p>${game.GoalAwai}</p>
-    </div>
-</div>
-</section>
-<section id="">
-<button id="butTeamHome">TeamHome</button>
-<button id="butTeamAwai">TeamAwai</button>
-</section>
-<section>
-<ul class="playersUl" id="playersHome">
-    <li>
-        <p>NOME</p>
-        <div id="actions">
-            <p>CARTÃO</p><button>-</button>
-            <p>${game}</p><button>+</button>
-            <p>GOLS</p><button>-</button>
-            <p>${game}</p><button>+</button>
-        </div>
-    </li>
-</ul>
-<ul class="playersUl" id="playersAwai">
-    <li>
-        <p>NOME</p>
-        <div id="actions">
-            <p>CARTÃO</p><button>-</button>
-            <p>${game}</p><button>+</button>
-            <p>GOLS</p><button>-</button>
-            <p>${game}</p><button>+</button>
-        </div>
-    </li>
-</ul>
-</section>
 <button id="finishGame">FINALIZAR PARTIDA</button>
 `)    
 
